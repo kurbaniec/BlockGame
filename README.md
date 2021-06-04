f1eba47a51ddc487d49eab1f2e23a7d5227b5ebb
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2021-06-04 19:57:08 +0200

    Set up OpenGL project with CMake Created functional application based on a simple solar system simulation with the CMake build tool. CMake looks for the OpenGL and GLUT packages/includes and links them to our application. Note: The application is shipped with GLUT in form of FreeGLUT which is found in the `dependencies` directory.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Block Game
+
+Work in progres..
+
+## IDE Support
+
+### Clion
+
+https://www.jetbrains.com/help/clion/creating-new-project-from-scratch.html
+
+### Visual Studio
+
+https://docs.microsoft.com/en-us/cpp/build/cmake-projects-in-visual-studio?view=msvc-160
+
+Note: You need put the `freeglut.dll` manually to the output directory when using Visual Studio because it is not using the standard CMake output.
+
---
+++ src/main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+int window;
+
+float hour = 0.0;
+float day = 0.0;
+float inc = 1.00;
+
+void resize(int width, int height)
+{
+    // prevent division by zero
+    if (height == 0) { height=1; }
+
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, (float)width/(float)height, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void keyPressed(unsigned char key, int x, int y)
+{
+    switch (key) {
+        case 27:
+            glutDestroyWindow(window);
+            exit(0);
+            break;
+    }
+}
+
+static void specialKeyPressed(int key, int x, int y)
+{
+    switch (key) {
+        case GLUT_KEY_UP:
+            inc *= 1.5;
+            break;
+        case GLUT_KEY_DOWN:
+            inc *= 0.75;
+            break;
+    }
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    hour += inc;
+    day += inc/24.0;
+    hour = hour - ((int)(hour/24))*24;
+    day = day - ((int)(day/365))*365;
+
+    glTranslatef (0.0, 0.0, -8.0);
+
+    glRotatef(360*day/365.0, 0.0, 1.0, 0.0);
+
+    // ecliptic
+    glRotatef(15.0, 1.0, 0.0, 0.0);
+
+    // sun
+    glColor3f(1.0, 1.0, 0.0);
+    glutWireSphere(1.0, 15, 15);
+
+    // earth
+    // position around the sun
+    glRotatef(360.0*day/365.0, 0.0, 1.0, 0.0);
+    glTranslatef(4.0, 0.0, 0.0);
+
+    glPushMatrix();
+    // rotate the earth on its axis
+    glRotatef(360.0*hour/24.0, 0.0, 1.0, 0.0);
+    glColor3f(1.0, 1.0, 1.0);
+    glutWireSphere(0.4, 10, 10);
+    glPopMatrix();
+
+    // moon
+    glRotatef(360.0*4*day/365.0, 0.0, 1.0, 0.0);
+    glTranslatef(0.7f, 0.0f, 0.0f);
+    glColor3f(0.3f, 0.7f, 0.3f);
+    glutWireSphere(0.1f, 10, 10);
+
+    glutSwapBuffers();
+}
+
+void init(int width, int height)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearDepth(1.0);
+    glDepthFunc(GL_LESS);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_FLAT);
+    resize(width, height);
+}
+
+/**
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}*/
+
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);
+    glutInitWindowSize(640, 480);
+    glutInitWindowPosition(0, 0);
+    window = glutCreateWindow("Block Game");
+    glutDisplayFunc(&display);
+    glutIdleFunc(&display);
+    glutReshapeFunc(&resize);
+    glutKeyboardFunc(&keyPressed);
+    glutSpecialFunc(&specialKeyPressed);
+    init(640, 480);
+    glutMainLoop();
+    return 0;
+}
+
+
+
